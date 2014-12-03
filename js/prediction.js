19a5c06210718669425b198ab5c185a9140d5174
shwetarameshk <skumar34@MPKL129147219.local>
2014-12-02 19:10:48 -0800

    Updated
    separated client and server side codes

--- js/prediction.js
+++ js/prediction.js
@@ -12,7 +12,7 @@ function submitForm(){
 	mitoses = document.getElementById("mitoses").value;
 
 	var predict_result = predictDiagnosis(clump_thick,cell_size,cell_shape, marg_adh,ep_cell_size,bare_nuclei,bl_chrom,norm_nuclei,mitoses);	
-	document.getElementById("predict_result").innerHTML = predict_result;
+	 document.getElementById("predict_result").innerHTML = predict_result;
 
 }
 
@@ -51,8 +51,52 @@ function predictDiagnosis (clump_thick,cell_size,cell_shape, marg_adh,ep_cell_si
 			}
 		}
 	}
-	document.getElementById('clumpthickness2').style.display = 'block';
-	document.getElementById('clumpthickness2').style.visibility = "visible";
+
+// 	var XHR = new XMLHttpRequest();
+// 	var urlEncodedData = {
+// 		clump_thick : clump_thick,
+// 		cell_size :cell_size,
+// 		cell_shape : cell_shape,
+// 		marg_adh : marg_adh,
+// 		ep_cell_size :ep_cell_size,
+// 		bare_nuclei :bare_nuclei,
+// 		bl_chrom :bl_chrom,
+// 		norm_nuclei :norm_nuclei,
+// 		mitoses : mitoses
+// 	};
+
+// 	var urlEncodedDataPairs = [];
+// 	var name;
+// 	XHR.open('POST', 'http://127.0.0.1:3000/predict/submitForm');
+
+//   // We add the required HTTP header to handle a form data POST request
+//   XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//   //XHR.setRequestHeader('Content-Length', urlEncodedData.length);
+
+//   // And finally, We send our data.
+//   XHR.send(urlEncodedData);
+
+// //var r = JSON.parse(XHR.responseText);	
+// //diagnosis = r.diagnosis;
+
+// XHR.onreadystatechange=function(){
+// 	if (XHR.readyState==4 && XHR.status==200){
+// 		   //string=xmlhttp.responseText;
+// 		   diagnosis = XHR.responseText;
+
+// 		   while(diagnosis=='' || diagnosis=='undefined' || diagnosis==null){
+// 		   	document.getElementById('predict_result').style.display = 'none';
+// 		   	document.getElementById('predict_result').style.visibility="hidden";          	
+// 		   }
+// 		   if (diagnosis!=''){
+// 		   	document.getElementById('predict_result').style.display = 'block';
+// 		   	document.getElementById('predict_result').style.visibility="visible";
+// 		   	document.getElementById("predict_result").innerHTML = diagnosis;
+// 		   }
+		   
+
+		   document.getElementById('clumpthickness2').style.display = 'block';
+		   document.getElementById('clumpthickness2').style.visibility = "visible";
 	// Set up the chart
 	var chart = new Highcharts.Chart({
 		chart: {
@@ -153,62 +197,62 @@ function predictDiagnosis (clump_thick,cell_size,cell_shape, marg_adh,ep_cell_si
 
 	document.getElementById('csizeu1').style.display = 'block';
 	document.getElementById('csizeu1').style.visibility = "visible";
-    //cellsize
-    var chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'csizeu1',
-            type: 'column',
-            margin: 75,
-            options3d: {
-                enabled: true,
-                alpha: 0,
-                beta: 0,
-                depth: 25,
-                viewDistance: 10
-            }
-        },
-        xAxis: {
-            title:{
-                text: 'Stats based on CellSizeUniformity'
-            },
-            categories: [1,2,3,4,5,6,7,8,9,10]
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Number of patients'
-            },
-            stackLabels: {
-                enabled: true,
-                style: {
-                    fontWeight: 'bold',
-                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                }
-            }
-        },
-        title: {
-            text: 'Stats based on CellSizeUniformity'
-        },
-        subtitle: {
-            text: ''
-        },
-        plotOptions: {
-            column: {
-                depth: 25
-            }
-        },
-        series: [{
-            name: 'Benign',
-            data: [369,37,27,8,0,0,1,1,9,0]
-        }, {
-            name: 'Malignant',
-            data: [4,8,25,30,30,25,18,27,5,67]
-        }]
-    });
+	//cellsize
+	var chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'csizeu1',
+			type: 'column',
+			margin: 75,
+			options3d: {
+				enabled: true,
+				alpha: 0,
+				beta: 0,
+				depth: 25,
+				viewDistance: 10
+			}
+		},
+		xAxis: {
+			title:{
+				text: 'Stats based on CellSizeUniformity'
+			},
+			categories: [1,2,3,4,5,6,7,8,9,10]
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: 'Number of patients'
+			},
+			stackLabels: {
+				enabled: true,
+				style: {
+					fontWeight: 'bold',
+					color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+				}
+			}
+		},
+		title: {
+			text: 'Stats based on CellSizeUniformity'
+		},
+		subtitle: {
+			text: ''
+		},
+		plotOptions: {
+			column: {
+				depth: 25
+			}
+		},
+		series: [{
+			name: 'Benign',
+			data: [369,37,27,8,0,0,1,1,9,0]
+		}, {
+			name: 'Malignant',
+			data: [4,8,25,30,30,25,18,27,5,67]
+		}]
+	});
 
 
-document.getElementById('cshapeu1').style.display = 'block';
-document.getElementById('cshapeu1').style.visibility = "visible";
+	document.getElementById('cshapeu1').style.display = 'block';
+	document.getElementById('cshapeu1').style.visibility = "visible";
 	// Set up the chart
 	var chart = new Highcharts.Chart({
 		chart: {
@@ -264,61 +308,61 @@ document.getElementById('cshapeu1').style.visibility = "visible";
 
 	document.getElementById('ma1').style.display = 'block';
 	document.getElementById('ma1').style.visibility = "visible";
-    //ma
-    var chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'ma1',
-            type: 'column',
-            margin: 75,
-            options3d: {
-                enabled: true,
-                alpha: 0,
-                beta: 0,
-                depth: 25,
-                viewDistance: 10
-            }
-        },
-        xAxis: {
-            title:{
-                text: 'MarginalAdhesion'
-            },
-            categories: [1,2,3,4,5,6,7,8,9,10]
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Number of patients'
-            },
-            stackLabels: {
-                enabled: true,
-                style: {
-                    fontWeight: 'bold',
-                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                }
-            }
-        },
-        title: {
-            text: 'Stats based on Marginal Adhesion'
-        },
-        subtitle: {
-            text: ''
-        },
-        plotOptions: {
-            column: {
-                depth: 25
-            }
-        },
-        series: [{
-            name: 'Benign',
-            data: [363, 37,31,5,4,3,0,0,0,1]
-        }, {
-            name: 'Malignant',
-            data: [30,21, 27, 28, 19,18,13,25,4,54]
-        }]
-    });
+	//ma
+	var chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'ma1',
+			type: 'column',
+			margin: 75,
+			options3d: {
+				enabled: true,
+				alpha: 0,
+				beta: 0,
+				depth: 25,
+				viewDistance: 10
+			}
+		},
+		xAxis: {
+			title:{
+				text: 'MarginalAdhesion'
+			},
+			categories: [1,2,3,4,5,6,7,8,9,10]
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: 'Number of patients'
+			},
+			stackLabels: {
+				enabled: true,
+				style: {
+					fontWeight: 'bold',
+					color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+				}
+			}
+		},
+		title: {
+			text: 'Stats based on Marginal Adhesion'
+		},
+		subtitle: {
+			text: ''
+		},
+		plotOptions: {
+			column: {
+				depth: 25
+			}
+		},
+		series: [{
+			name: 'Benign',
+			data: [363, 37,31,5,4,3,0,0,0,1]
+		}, {
+			name: 'Malignant',
+			data: [30,21, 27, 28, 19,18,13,25,4,54]
+		}]
+	});
 
-document.getElementById('bn1').style.display = 'block';
-document.getElementById('bn1').style.visibility = "visible";
+	document.getElementById('bn1').style.display = 'block';
+	document.getElementById('bn1').style.visibility = "visible";
 	// Set up the chart
 	var chart = new Highcharts.Chart({
 		chart: {
@@ -374,57 +418,64 @@ document.getElementById('bn1').style.visibility = "visible";
 
 	document.getElementById('nn1').style.display = 'block';
 	document.getElementById('nn1').style.visibility = "visible";
-    var chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'nn1',
-            type: 'column',
-            margin: 75,
-            options3d: {
-                enabled: true,
-                alpha: 0,
-                beta: 0,
-                depth: 25,
-                viewDistance: 10
-            }
-        },
-        xAxis: {
-            title:{
-                text: 'NormalNucleoli'
-            },
-            categories: [1,2,3,4,5,6,7,8,9,10]
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Number of patients'
-            },
-            stackLabels: {
-                enabled: true,
-                style: {
-                    fontWeight: 'bold',
-                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                }
-            }
-        },
-        title: {
-            text: 'Stats based on NormalNucleoli'
-        },
-        subtitle: {
-            text: ''
-        },
-        plotOptions: {
-            column: {
-                depth: 25
-            }
-        },
-        series: [{
-            name: 'Benign',
-            data: [391,30,11,1,2,4,2,3,0,0]
-        }, {
-            name: 'Malignant',
-            data: [41,6,31,17,17,18,14,20,15,60]
-        }]
-    });
+	var chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'nn1',
+			type: 'column',
+			margin: 75,
+			options3d: {
+				enabled: true,
+				alpha: 0,
+				beta: 0,
+				depth: 25,
+				viewDistance: 10
+			}
+		},
+		xAxis: {
+			title:{
+				text: 'NormalNucleoli'
+			},
+			categories: [1,2,3,4,5,6,7,8,9,10]
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: 'Number of patients'
+			},
+			stackLabels: {
+				enabled: true,
+				style: {
+					fontWeight: 'bold',
+					color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+				}
+			}
+		},
+		title: {
+			text: 'Stats based on NormalNucleoli'
+		},
+		subtitle: {
+			text: ''
+		},
+		plotOptions: {
+			column: {
+				depth: 25
+			}
+		},
+		series: [{
+			name: 'Benign',
+			data: [391,30,11,1,2,4,2,3,0,0]
+		}, {
+			name: 'Malignant',
+			data: [41,6,31,17,17,18,14,20,15,60]
+		}]
+	});
 
-return diagnosis;
+	return diagnosis;
+}
+}
+
+
+
+
+
 }
